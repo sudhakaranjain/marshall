f3b3e14839379c875579941b1a40073528ae2f94
Sudhakaran Jain <sudhakar.jayasenan.jain@slimmer.ai>
2023-03-14 01:14:06 +0100

    changing feature extractors to linear projections

--- marshall/marshall.py
+++ marshall/marshall.py
@@ -1,12 +1,13 @@
 from typing import Any, List, Union
+import math
 
+from torch.autograd import Variable
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 from einops.layers.torch import Rearrange
-from transformers import AutoModel
-
+from transformers import AutoModel, AutoTokenizer
 
 from marshall.ema import EMA
 
@@ -16,7 +17,8 @@ class ImageFeatureExtractor(nn.Module):
         """
         Initializes the pretrained resnet18 model to be used as feature extractor.
 
-        :param patch_size: dimension for each patch created from the image 
+        :param patch_size: dimension for each patch created from the image
+        :param hidden_size: dimension for projection embedding
         :param freeze: Whether to freeze the model so that it is not updated during backprop
         """
         super(ImageFeatureExtractor, self).__init__()
@@ -39,7 +41,7 @@ class ImageFeatureExtractor(nn.Module):
         features = torch.stack([self.model(y) for y in x])
         if average:
             return torch.mean(self.image_embedding(features), 1)
-        return self.image_embedding(features) 
+        return self.image_embedding(features)
 
 
 class TextFeatureExtractor(nn.Module):
@@ -70,21 +72,82 @@ class TextFeatureExtractor(nn.Module):
         return model_output.last_hidden_state
 
 
+class ImageEmbedder(nn.Module):
+    def __init__(self, patch_size: int, hidden_size: int):
+        """
+        Initializes the pretrained resnet18 model to be used as feature extractor.
+
+        :param patch_size: dimension for each patch created from the image
+        :param hidden_size: dimension for projection embedding
+        """
+        super(ImageEmbedder, self).__init__()
+
+        self.patch_n_flatten_layer = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size)
+        self.patch_projection = nn.Linear(patch_size * patch_size * 3, hidden_size)
+
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        """
+        Gets the representation batch i.e. the output for the input batch of images.
+
+        :param image: input batch images that needs to be processed (patching and feature extraction)
+        """
+        x = self.patch_n_flatten_layer(image)
+        return self.patch_projection(x)
+
+
+class TextEmbedder(nn.Module):
+    def __init__(self, pretrained_model, hidden_size: int, vocab_size: int = 30522, max_seq_len: int = 512):
+        """
+        :param hidden_size: dimension for projection embedding
+        """
+        super(TextEmbedder, self).__init__()
+        self.hidden_size = hidden_size
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+        self.embed = nn.Embedding(vocab_size, hidden_size)
+
+        # Positional Encoding:
+        # create constant 'pe' matrix with values dependent on pos and i
+        pe = torch.zeros(max_seq_len, hidden_size)
+        for pos in range(max_seq_len):
+            for i in range(0, hidden_size, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / hidden_size)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1)) / hidden_size)))
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, text_batch: torch.Tensor, ) -> torch.Tensor:
+        """
+        Gets the representation batch i.e. the pooled output for the input batch  of tokens.
+
+        :param text_batch:
+        """
+        x = self.tokenizer(text_batch, padding=True, truncation=True, return_tensors="pt")['input_ids']
+        x = self.embed(x)
+        x = x * math.sqrt(self.hidden_size)  # make embeddings relatively larger
+        seq_len = x.size(1)  # add constant to embedding
+        return x + Variable(self.pe[:, :seq_len], requires_grad=False)
+
+
 class MultiModalEncoder(nn.Module):
     def __init__(self, config):
         """
         Initializes a Multimodal Encoder with modality specific feature extractor and a fusion layer w.r.t the params
         in the config.
 
-        :param config: Config file containing various hyperparameters needed for initializing the MMEncoder
+        :param config: Config file containing various hyper parameters needed for initializing the MMEncoder
         """
         super(MultiModalEncoder, self).__init__()
         self.hidden = config.model.hidden_dim
-        self.image_feature_extractor = ImageFeatureExtractor(patch_size=config.dataset.patch_size,
-                                                             hidden_size=config.model.hidden_dim,
-                                                             freeze=config.model.vision.freeze)
-        self.caption_feature_extractor = TextFeatureExtractor(pretrained_model=config.model.text.pretrained_model,
-                                                              freeze=config.model.text.freeze)
+        # self.image_embedder = ImageFeatureExtractor(patch_size=config.dataset.patch_size,
+        #                                                      hidden_size=config.model.hidden_dim,
+        #                                                      freeze=config.model.vision.freeze)
+        # self.caption_embedder = TextFeatureExtractor(pretrained_model=config.model.text.pretrained_model,
+        #                                                       freeze=config.model.text.freeze)
+
+        self.image_embedder = ImageEmbedder(patch_size=config.dataset.patch_size, hidden_size=config.model.hidden_dim)
+        self.caption_embedder = TextEmbedder(pretrained_model=config.model.text.pretrained_model,
+                                             hidden_size=config.model.hidden_dim)
 
         # Fusion layer
         # TODO: Experiment with different fusion layer
@@ -124,8 +187,8 @@ class MultiModalEncoder(nn.Module):
         :returns: representation vectors of the input batch
         """
         is_pooled_features = True if self.fusion_layer_type == 'mlp' else False
-        features = self.image_feature_extractor(x, average=is_pooled_features) if modality == 'vision' \
-            else self.caption_feature_extractor(x, pooling=is_pooled_features)
+        features = self.image_embedder(x) if modality == 'vision' \
+            else self.caption_embedder(x)
 
         if self.fusion_layer_type == 'transformer':
             output = []
